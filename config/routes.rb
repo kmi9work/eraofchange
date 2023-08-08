7f09a7098319db50d4851304beb4151a46b7d800
KostenchukMI <kostenchukmi@mosreg.ru>
2023-08-08 15:48:40 +0300

    task N 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/settlements', to: 'settlements#index'
-  get '/settlements/new', to: 'settlements#new'
-  post '/settlements/create', to: 'settlements#create'
+  get '/settlements/new', to: 'settlements#new', as: :new_settlement
+  post '/settlements', to: 'settlements#create'
   get '/settlements/:id', to: 'settlements#show', as: :settlement
   get '/settlements/edit/:id', to: 'settlements#edit'
   get '/settlements/destroy/:id', to: 'settlements#destroy'
