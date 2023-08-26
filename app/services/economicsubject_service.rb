6894ae5e0c1ac0c80b6987603d219032f51289a7
anton.s1987 <anton.s1987@yandex.ru>
2023-08-26 20:23:09 +0300

    removes excess tabs

--- app/services/economicsubject_service.rb
+++ app/services/economicsubject_service.rb
@@ -1,29 +1,29 @@
 class EconomicsubjectService
-	attr_accessor :name, :category, :property
-	def self.all
-		f = File.open("db/my_db/economicsubject.csv", "r")
-		str = f.gets
-		
-		economicsubjects = []
+  attr_accessor :name, :category, :property
+  def self.all
+    f = File.open("db/my_db/economicsubject.csv", "r")
+    str = f.gets
+    
+    economicsubjects = []
 
-		while (str.present?)
-			economicsubject = EconomicsubjectService.new
-			economicsubject.name =  str.split(";")[0]
-			economicsubject.category = str.split(";")[1]
-			economicsubject.property = str.split(";")[2]
-				
-				#if str.split(";")[1] == "town\n"
-				economicsubjects.push(economicsubject)
-				#end
+    while (str.present?)
+      economicsubject = EconomicsubjectService.new
+      economicsubject.name =  str.split(";")[0]
+      economicsubject.category = str.split(";")[1]
+      economicsubject.property = str.split(";")[2]
+        
+        #if str.split(";")[1] == "town\n"
+        economicsubjects.push(economicsubject)
+        #end
 
-			str = f.gets
-		end
-		f.close
-		
+      str = f.gets
+    end
+    f.close
+    
 
-		economicsubjects
-		
-	end
+    economicsubjects
+    
+  end
 
 end
 
