1ae8db2cffbe72c3d5d5610f4449254fd9208df0
KostenchukMI <kostenchukmi@mosreg.ru>
2023-10-21 18:30:54 +0300

    fix

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,6 @@
 class Merchant < ApplicationRecord
   has_one :player
-  has_one :family
+  belongs_to :family
   belongs_to :guild, optional: true
   has_many :plants, :as => :economic_subject,
                     :inverse_of => :economic_subject
