fb4c9980b8f10eb08aa0b0ea4ae3fb899c6298c2
Dasha <dashynya0889@mail.ru>
2024-11-13 16:12:57 +0300

    political_action_type_1

--- app/models/political_action_type.rb
+++ app/models/political_action_type.rb
@@ -61,8 +61,9 @@ class PoliticalActionType < ApplicationRecord
     # В options[:country_id] должна лежать страна контрабанды
     if success
       player = Player.find_by_id(options[:player_id])
+      country = Country.find_by_id(options[:country_id])
       if player
-        player.params["contraband"].push(options[:country_id])
+        player.params["contraband"].push(country.title)
         player.save
       end
     end
@@ -82,5 +83,164 @@ class PoliticalActionType < ApplicationRecord
   
   end
 
-end
+  def send_embassy(success, options) #Отправить посольство
+    if success
+      player = Player.find_by_id(options[:player_id])
+      countries = Country.find(options[:country_ids])
+      if player && countries
+        player.params["infuence"] += 1
+        player.save
+        countries.each do |c|
+          c.params["relations"] += 1
+          c.save
+        end
+      end
+    end
+  end
 
+  def equip_caravan(success, options) #Снарядить караван
+    player = Player.find_by_id(options[:player_id])
+    if success
+      player.params["infuence"] += 2
+      player.save
+    else
+      player.params["infuence"] -= 2
+      player.save
+    end   
+  end
+
+  def take_bribe(success, options) #Взять мзду
+    player = Player.find_by_id(options[:player_id])
+    country = Country.find_by_id(rand(2..7))
+    if success
+      player.params["infuence"] += 1
+      player.save
+    else
+      player.params["infuence"] -= 3
+      player.save
+      country.params["relations"] -= 1
+      country.save
+    end
+  end
+
+  def сonduct_audit(success, options) #Провести ревизию
+    if success
+      player = Player.find_by_id(options[:player_id])
+      if player
+        player.params["infuence"] += 1
+        player.save
+      end
+    end
+  end
+
+  def peculation(success, options) #Казнокрадство
+    player = Player.find_by_id(options[:player_id])
+    if success
+      player.params["infuence"] += 2
+      player.save
+    else
+      player.params["infuence"] -= 3
+      player.save
+    end
+  end
+
+  def disperse_bribery(success, options) #Разогнать мздоимцев
+    player = Player.find_by_id(options[:player_id])
+    regions = Country.find(Country::RUS).regions
+    if success
+      player.params["infuence"] += 3
+      player.save
+    else
+      player.params["infuence"] -= 3
+      player.save
+      regions.each do |r|
+        r.params["public_order"] -= 5
+        r.save
+      end 
+    end
+  end
+
+  def implement_sabotage(success, options) #Осуществить саботаж
+    if success
+      player = Player.find_by_id(options[:player_id])
+      if player
+        player.params["infuence"] += 1
+        player.save
+      end
+    end
+  end
+
+  def name_of_grand_prince(success, options) #Именем Великого князя!
+    player_1 = Player.find_by_id(options[:player_id])
+    player_2 = Player.find_by_id(options[:job_id])
+    if success
+      player_1.params["infuence"] += 1
+      player_1.save
+    else
+      player_1.params["infuence"] -= 5
+      player_1.save
+      player_2.params["infuence"] -= 3
+      player_2.save
+    end
+  end
+
+  def recruiting(success, options) #Набрать рекрутов
+    player = Player.find_by_id(options[:player_id])
+    if success
+      player.params["infuence"] += 1
+      player.save
+    else
+      player.params["infuence"] -= 5
+      player.save
+
+    end
+  end
+
+  def mobile_court(success, options) #Выездной суд
+    if success
+      region = Region.find_by_id(options[:region_id])
+      player = Player.find_by_id(options[:player_id])
+      if region && player
+        region.params["public_order"] += 5
+        region.save
+        player.params["infuence"] += 1
+        player.save
+      end
+    end
+  end
+
+  def fabricate_denunciation(success, options) #Сфабриковать донос
+  end
+
+  def legislative_reform(success, options)#Законодательная реформа
+  end
+
+  def sermon(success, options)#Проповедь    
+  end
+
+  def eradicate_heresies(success, options) #Искоренить ереси
+  end
+
+  def call_unity(success, options) #Призыв к единству
+  end
+
+  def regency(success, options) #Регентство
+  end
+
+  def use_seal(success, options) #Использовать великокняжескую печать
+  end
+
+  def favoritism(success, options) #Фаворитизм
+  end
+
+  def development_farm(success, options) #Развитие хозяйства
+  end
+
+  def confused_his_with_state(success, options) #Спутал свое с государственным
+  end
+
+  def patronage_gentiles(success, options) #Покровительство иноверцам
+  end
+
+
+end
