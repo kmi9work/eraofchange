fc87774cd277891704d87eb43dcc2350300068a1
Anton-s1987 <petitlapin1987@gmail.com>
2025-06-27 00:50:02 +0300

    deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,21 +5,37 @@ set :application, "eraofchange"
 set :repo_url, "git@github.com:kmi9work/eraofchange.git"
 set :branch, 'depl'
 
-set :deploy_to, "/home/deploy/eraofchange"
-# Passenger
-set :passenger_restart_with_touch, true
-set :passenger_ruby, '/home/deploy/.rbenv/shims/ruby'
+set :deploy_to, "/opt/era/eraofchange"
 
+# Passenger
+#set :passenger_restart_with_touch, true
+set :passenger_ruby, '/home/deploy/.rbenv/shims/ruby'
 #set :passenger_restart_command, 'passenger-config restart-app'
 
 set :rbenv_type, :user  # или :system, если Ruby установлен системно
 set :rbenv_ruby, '3.2.2'  # Замените на вашу версию Ruby
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 
+set :keep_releases, 3
 
+set :default_env, { 
+  'RAILS_MASTER_KEY' =>               File.read('config/master.key').strip,
+  'ERAOFCHANGE_DATABASE_PASSWORD' =>  File.read('config/database.key').strip
+}
+
+#бэкенд  + фронтенд
+namespace :deploy do
+  desc "Полный деплой (бэкенд  + фронтенд)"
+  task :full do
+    invoke 'deploy'      # Стандартный деплой Capistrano
+    
+    invoke 'frontend:deploy' # Сборка фронтенда
+  end
+end
+
+#Разворачивание бека с нуля
 #Команада cap production deploy
 namespace :deploy do
-  # Задача для создания папок и файлов при первом деплое
   task :setup_config do
     on roles(:app) do
       # Создаем необходимые папки в shared
@@ -35,19 +51,16 @@ namespace :deploy do
       
       # Устанавливаем правильные права
       execute :chmod, "640 #{shared_path}/config/master.key" if test("[ -f #{shared_path}/config/master.key ]")
-      execute :touch, "#{current_path}/tmp/restart.txt"
+     # execute :touch, "#{current_path}/tmp/restart.txt"
     end
-  end
-
-  
-  
+  end  
 
   # Хуки для выполнения задач
   before 'deploy:check:linked_files', 'deploy:setup_config'
   after 'deploy:publishing', 'deploy:restart'
 end
 
-
+#Перезалив базы
 #Команада cap production db:reinit
 namespace :db do
   desc 'Полный сброс и инициализация БД (drop, create, migrate, seed)'
@@ -55,12 +68,9 @@ namespace :db do
     on roles(:db) do
       within release_path do
         with rails_env: fetch(:rails_env) do
-               # Получаем параметры подключения из database.yml
+          # Получаем параметры подключения из database.yml
           db_name = 'eraofchange_production'
-          db_user = 'deploy'  # Замените на реального пользователя
-
-          # Проверяем, что подключение к PostgreSQL есть
-          execute :psql, "-U  #{db_user}  -d postgres, '--version' "
+          db_user = 'deploy'
 
           # 1. Дропаем базу (если существует)
           execute :psql, "-U #{db_user} -c 'DROP DATABASE IF EXISTS #{db_name}' postgres"
@@ -73,9 +83,6 @@ namespace :db do
 
           # 4. Заполняем данными
           execute :rake, 'db:seed:all'
-
-          # 5. Проверяем
-          execute :psql, "-U #{db_user} -c '\\l' #{db_name} | grep #{db_name}"
         end
       end
     end
@@ -85,18 +92,23 @@ end
 append :linked_files, "config/database.yml", 'config/master.key'
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
 
+
+#Установка фронта
+#Команда cap production frontend:deploy
+#TO DO: Сделать структуру папок как у бека (с номерами релизов и текущей версией)
 namespace :frontend do
   task :deploy do
     on roles(:app) do
       frontend_dir = "/opt/era/era_front"
       execute :git, :clone, "--depth 1", "git@github.com:kmi9work/era_front.git", frontend_dir unless test("[ -d #{frontend_dir} ]")
       within frontend_dir do
-        execute :git, :pull
+        #execute :git, :pull
         execute :bash, '-lc', '"source ~/.nvm/nvm.sh && nvm install v22"'
         execute :bash, "-c", '"curl -fsSL https://get.pnpm.io/install.sh | sh -"'
 
         # Установка зависимостей через pnpm
         execute :bash, '-lc', '"source ~/.nvm/nvm.sh && export PATH=$HOME/.nvm/versions/node/v22/bin:$HOME/.local/share/pnpm:$PATH && pnpm install --ignore-scripts"'
+        
         # Замена адреса
         execute :sed, '-i', "'s|VITE_PROXY=http://localhost:3000|VITE_PROXY=https://epoha.igroteh.su/backend|g'", "#{frontend_dir}/.env"
 
@@ -106,25 +118,3 @@ namespace :frontend do
     end
   end
 end
-
-
-
-
-
-set :keep_releases, 3
-
-set :default_env, { 
-  'RAILS_MASTER_KEY' => File.read('config/master.key').strip,
-  'ERAOFCHANGE_DATABASE_PASSWORD' => 'b1cf3cdaf6066b' #для теста
-}
-
-
-  #  after :migrate, :seed do
-  #   on primary :db do
-  #     within release_path do
-  #       with rails_env: fetch(:stage) do
-  #         execute :rake, 'db:seed:all'
-  #       end
-  #     end
-  #   end
-  # end
