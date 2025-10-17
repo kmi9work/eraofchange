df9d6a94a0799f793ed74b3d29d986ccdac76f97
Michail <kmi9.mail@gmail.com>
2025-10-17 21:07:37 +0300

    private fix

--- app/models/player.rb
+++ app/models/player.rb
@@ -284,6 +284,47 @@ class Player < ApplicationRecord
     influence_buildings + influence_items.sum{|ii| ii.value.to_i}
   end
 
+  def give_credit(plant_ids)
+    if check_credit(plant_ids)
+      plants = Plant.where(id: plant_ids)
+      credit_deposit = 0
+      credit_term = GameParameter.find_by(identificator: "credit_term")&.value.to_i
+      credit_size = GameParameter.find_by(identificator: "credit_size")&.value.to_f / 100
+      start_year = GameParameter.find_by(identificator: "current_year")&.value.to_i
+      plants.each { |p| credit_deposit += p.plant_level.deposit }
+      credit_sum = credit_deposit + ((credit_deposit * credit_size) * credit_term).round if credit_deposit.present?
+      credit_new = self.credits.create(sum: credit_sum, deposit: credit_deposit, procent: credit_size, duration: credit_term, start_year: start_year)
+      new_credit_id = credit_new.id
+      plants_with_credit = plants.update(credit_id: new_credit_id)
+      final_hash = {
+        credit_deposit: credit_deposit, 
+        credit_term: credit_term,
+        credit_sum: credit_sum
+      }
+      return {result: final_hash, msg: "Кредит выдан"}
+    else
+      return {result: false, msg: "Кредит не выдан, одно или несколько предприятий уже находятся в залоге"}
+    end
+  end
+
+  def add_army(army_size_id, region_id)
+    self.armies.create(army_size_id: army_size_id, region_id: region_id)
+  end
+
+  def run_political_action(political_action_type_id, year, success, options)
+    pat = PoliticalActionType.find_by_id(political_action_type_id)
+    result = pat.execute(success, options)
+    self.political_actions.create(year: year, success: success, params: result, political_action_type_id: political_action_type_id)
+  end
+
+  def self.all_contrabandists
+    Player.all.select{|p| p.params["contraband"]&.include?(GameParameter.current_year)}
+  end
+
+  def modify_influence(value, comment, entity) #Изменить влияние игрока
+    InfluenceItem.add(value, comment, self, entity)
+  end
+
   private
 
   # Проверка достаточности золота для покупки ресурсов
@@ -372,47 +413,5 @@ class Player < ApplicationRecord
     plants = Plant.where(id: plant_ids)
     plants.all?{|p| p.credit_id.blank?}
   end
-
-  def give_credit(plant_ids)
-    if check_credit(plant_ids)
-      plants = Plant.where(id: plant_ids)
-      credit_deposit = 0
-      credit_term = GameParameter.find_by(identificator: "credit_term")&.value.to_i
-      credit_size = GameParameter.find_by(identificator: "credit_size")&.value.to_f / 100
-      start_year = GameParameter.find_by(identificator: "current_year")&.value.to_i
-      plants.each { |p| credit_deposit += p.plant_level.deposit }
-      credit_sum = credit_deposit + ((credit_deposit * credit_size) * credit_term).round if credit_deposit.present?
-      credit_new = self.credits.create(sum: credit_sum, deposit: credit_deposit, procent: credit_size, duration: credit_term, start_year: start_year)
-      new_credit_id = credit_new.id
-      plants_with_credit = plants.update(credit_id: new_credit_id)
-      final_hash = {
-        credit_deposit: credit_deposit, 
-        credit_term: credit_term,
-        credit_sum: credit_sum
-      }
-      return {result: final_hash, msg: "Кредит выдан"}
-    else
-      return {result: false, msg: "Кредит не выдан, одно или несколько предприятий уже находятся в залоге"}
-    end
-  end
-
-  def add_army(army_size_id, region_id)
-    self.armies.create(army_size_id: army_size_id, region_id: region_id)
-  end
-
-  def run_political_action(political_action_type_id, year, success, options)
-    pat = PoliticalActionType.find_by_id(political_action_type_id)
-    result = pat.execute(success, options)
-    self.political_actions.create(year: year, success: success, params: result, political_action_type_id: political_action_type_id)
-  end
-
-  def self.all_contrabandists
-    Player.all.select{|p| p.params["contraband"]&.include?(GameParameter.current_year)}
-  end
-
-  def modify_influence(value, comment, entity) #Изменить влияние игрока
-    InfluenceItem.add(value, comment, self, entity)
-  end
-
 end
 
