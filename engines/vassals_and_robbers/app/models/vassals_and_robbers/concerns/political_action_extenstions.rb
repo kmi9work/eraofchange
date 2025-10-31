6c6335bf27917c7e428c9e44d766fc34504ae5f0
Anton-s1987 <petitlapin1987@gmail.com>
2025-10-31 20:04:10 +0300

    dd

---
+++ engines/vassals_and_robbers/app/models/vassals_and_robbers/concerns/political_action_extenstions.rb
@@ -0,0 +1,42 @@
+module VassalsAndRobbers
+  module Concerns
+    module PoliticalActionExtensions
+      extend ActiveSupport::Concern
+
+      included do
+      end
+
+      class_methods do
+
+        def support_export
+
+        end
+
+      end
+    end
+  end
+end
+
+# Поддержать экспорт
+# Личный визит
+# Передача армии
+
+# Инвестиции
+# Передача государевых подвод
+
+# Покровительство иноверцам
+# Внедрение инноваций
+
+# Отправить посольство
+# Заключить союз
+# Принести дары
+
+# Призыв к единству
+# Проповедь
+# Принести дары
+
+# Набрать рекрутов
+# Защита каравана
+
+# Контрразведка
+# Разведка
