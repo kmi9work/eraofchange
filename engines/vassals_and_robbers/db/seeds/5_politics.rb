815b7ead15e5cb0ac15c3ce7a1975f7b5cff56e8
Michail <kmi9.mail@gmail.com>
2026-04-17 19:21:53 +0300

    fix

---
+++ engines/vassals_and_robbers/db/seeds/5_politics.rb
@@ -0,0 +1,15 @@
+PoliticalActionType.destroy_all
+
+pat_path = './db/seeds/pat_nobles_vassals.csv'
+
+
+f = File.open(pat_path, 'r+')
+f.gets #headers
+while str = f.gets
+  job_name, name, action, icon, desc, prob, cost, success, failure = str.split(";").map{|i| i.strip}
+  job = Job.find_by_name(job_name)
+  PoliticalActionType.create(
+    icon: icon, name: name, action: action, job: job,
+    description: desc, cost: cost, probability: prob, 
+    success: success, failure: failure)
+end
